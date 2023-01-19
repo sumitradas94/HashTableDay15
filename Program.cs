c46a3c0c7088b751b79644a4fafdaaa04826947f
sumitradas94 <sumitradas94@gmail.com>
2023-01-19 21:47:54 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HashTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            LinkedHashMap<string, int> linked_hash_map = new LinkedHashMap<string, int>(5);
+
+            string Sentence = "to be or not to be";
+            string[] Words = Sentence.Split(" ");
+            foreach (string word in Words)
+            {
+                int Value = linked_hash_map.Get(word);
+                if (Value == default)
+                    Value = 1;
+                else Value++;
+                linked_hash_map.Add(word, Value);
+            }
+            int Frequency = linked_hash_map.Get("to");
+            Console.WriteLine("Frquency of word in mentioned statement is : " + Frequency);
+        }
+    }
+}
