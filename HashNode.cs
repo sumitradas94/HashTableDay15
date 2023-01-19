734a1d40a63049448c3601ccb3b5b4ff38879ae2
sumitradas94 <sumitradas94@gmail.com>
2023-01-19 21:45:32 +0530

    Define Node Constructor

---
+++ HashNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class HashNode<K, V>
+    {
+        public K Key;
+        public V Value;
+        public HashNode<K, V> Next;
+
+        public HashNode(K Key, V Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+            Next = null;
+        }
+    }
+}
